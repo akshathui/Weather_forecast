1e9a6c494d20e87d6e09bc7fa9b7ce89a572fe8a
akshathui <akshatkant12@gmail.com>
2025-07-11 14:17:26 +0530

    Update Cards component to show only time in hh:mm format

--- src/components/Aside.jsx/Cards.jsx
+++ src/components/Aside.jsx/Cards.jsx
@@ -10,9 +10,15 @@ const Cards = () => {
       {hourly.slice(0, 6).map((hour, index) => (
         <div
           key={index}
-          className="shadow-xl hover:shadow-xl/20 h-max p-4 rounded-2xl flex flex-col justify-center items-center"
-        >
-          <h3 className="text-lg font-light">{hour.time}</h3>
+          className="shadow-xl flex-wrap hover:shadow-xl/20 h-max p-4 rounded-2xl flex flex-col justify-center items-center"
+         >
+          <h3 className="text-lg font-light">
+            {new Date(hour.date).toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: false, // Set to true if you want 12-hour format
+            })}
+          </h3>
           <h1 className="text-xl font-normal ml-2">{hour.temperature}°</h1>
           <h4 className="text-md font-extralight">{hour.summary}</h4>
         </div>
